e7ec3e5daebe4a4a438e82a37c7c172cb13c9941
risarahayu <63339223+risarahayu@users.noreply.github.com>
2024-12-08 17:22:24 +0800

    new look

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+  gsap.to(".container-bg-2", {
+    backgroundPosition: "center 50px", // Bergerak vertikal
+    repeat: -1, // Ulang terus
+    yoyo: true, // Bergerak naik-turun
+    duration: 5, // Durasi animasi
+    ease: "power1.inOut"
+  });
+
+  gsap.to(".container-bg", {
+    backgroundPosition: "center 50px", // Bergerak vertikal
+    repeat: -1, // Ulang terus
+    yoyo: true, // Bergerak naik-turun
+    duration: 5, // Durasi animasi
+    ease: "power1.inOut"
+  });
+
+  gsap.to(".container-bg-3", {
+    backgroundPosition: "center 50px", // Bergerak vertikal
+    repeat: -1, // Ulang terus
+    yoyo: true, // Bergerak naik-turun
+    duration: 5, // Durasi animasi
+    ease: "power1.inOut"
+  });
